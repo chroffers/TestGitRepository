06124adc74e4728f94cf13095fc8867d20549632
chris <chris@192.168.1.2>
2016-08-13 13:49:46 +0200

    2eme co

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,6 +9,7 @@ and open the template in the editor.
         
         <h1> CA MARCHE </h1>
         
+        <h2> deuxième modification </h2>
         <?php
         // put your code here
         ?>
